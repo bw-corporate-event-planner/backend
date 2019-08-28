eb41aab9020bc9c4f8e45ecb1b7ddbea1a233d15
unknown <samantha.c.egge@gmail.com>
2019-08-28 06:42:47 -0700

    Created vendor model and routes.

--- vendors/vendors-model.js
+++ vendors/vendors-model.js
@@ -3,3 +3,32 @@ const knexConfig = require('../knexfile.js')
 const environment = process.env.DB_ENV || 'development';
 
 const db = knex(knexConfig[environment])
+
+module.exports = {
+  findVendors,
+  findVendorID,
+  createVendor
+}
+
+function findVendors() {
+  return db('vendors')
+    .select('*')
+}
+
+function findVendorID(id) {
+  return db('vendors')
+    .where({ id })
+    .first()
+    .select('*')
+}
+
+function createVendor(vendor) {
+  return db('vendors')
+    .insert(vendor, 'id')
+    .then(ids => {
+      const [id] = ids
+      return findVendorID(id)
+        .first()
+        .select('*')
+    })
+}
